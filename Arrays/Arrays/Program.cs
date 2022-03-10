b2c5ea318a80f7632979b6a924fdb366029ab532
Diash177 <Diash177@gmail.com>
2022-03-11 01:03:09 +0500

    Sketch Hw_Arrays
    ^-^

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Homework;
+
+
+/* Test FindSumElementsOfOddDigits */
+//int[] a = Arrays.GenerateArray(5);
+//Arrays.WriteArray(a);
+//int sum = Arrays.FindSumElementsOfOddDigits(a);
+//Console.WriteLine(sum);
+
+
+/* Test DoReverseOfArray */
+//int[] a = Arrays.GenerateArray(5);
+//Arrays.WriteArray(a);
+//a = Arrays.DoReverseOfArray(a);
+//Arrays.WriteArray(a);
+
+/* Test ChangePlacesOfTwoPartsOfArrange */
+int[] a = Arrays.GenerateArray(4);
+Arrays.WriteArray(a);
+a = Arrays.ChangePlacesOfTwoPartsOfArrange(a);
+Arrays.WriteArray(a);
