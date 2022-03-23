354899a6adc4d8c61be649059e8a688d1ddb3c91
Diash177 <Diash177@gmail.com>
2022-03-23 19:36:49 +0500

    Re-upload homework

---
+++ Arrays.Test/ArraysTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+
+namespace OneDimensionalArrayTests
+{
+    public class OneDimensionalArrayTests
+    {
+        public void GenerateArrayTest(int lenght, int[] expected)
+        {
+             int[] actual = Arrays.OneDimensionalArray.GenerateArray(lenght);
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿
+//class TestClass
+//{
+//    static void Main(string[] args)
+//    {
+//        /* Test FindSumElementsOfOddDigits */
+//        //int[] a = Arrays.GenerateArray(5);
+//        //Arrays.WriteArray(a);
+        //int sum = Arrays.FindSumElementsOfOddDigits(a);
+        //Console.WriteLine(sum);
+
+        /* Test DoReverseOfArray */
+        //int[] a = Arrays.GenerateArray(5);
+        //Arrays.WriteArray(a);
+        //a = Arrays.DoReverseOfArray(a);
+        //Arrays.WriteArray(a);
+
+        ///* Test ChangePlacesOfTwoPartsOfArrange */
+        //int[] a = Arrays.GenerateArray(4);
+        //Arrays.WriteArray(a);
+        //a = Arrays.ChangePlacesOfTwoPartsOfArrange(a);
+        //Arrays.WriteArray(a);
+
+        /* Test DoBubbleSort */
+        //int[] a = OneDimensionalArray.Arrays.GenerateArray(10);
+        //OneDimensionalArray.Arrays.WriteArray(a);
+        //a = OneDimensionalArray.Arrays.DoBubbleSort(a);
+        //OneDimensionalArray.Arrays.WriteArray(a);
+
+        /* Test FindMaxElement */
+        //int[,] a = TwoDimensionalArray.Arrays.GenerateArray(3, 3);
+        //TwoDimensionalArray.Arrays.PrintArray(a);
+        //int max = TwoDimensionalArray.Arrays.FindMaxElement(a);
+        //Console.WriteLine("Max element = " + max);
+
+        /* Test FindNumberOfTheBiggestElementAroundNeighbours */
+//        int[,] a = TwoDimensionalArray.Arrays.GenerateArray(3, 3);
+//        TwoDimensionalArray.Arrays.PrintArray(a);
+//        int count = TwoDimensionalArray.Arrays.FindNumberOfTheBiggestElementAroundNeighbours(a);
+//        Console.WriteLine($"count={count}");
+//    }
+//}
